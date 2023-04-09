c0b407185e5a5a1db14a923da2b860b0cc1e8717
khareenp <khareenp1@gmail.com>
2023-04-09 19:59:12 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,12 +22,13 @@ const About = () => {
         </p>
         <br />
         <p className="text-xl">
-          My frontend technology of choice is ReactJS but I do have some
-          experience with VueJS as well. For DBMS I have experience in both SQL
-          or NOSQL databases using MongoDB and Postgresql. For serverside
-          development I have experience in NodeJS, Golang and I am currently
-          learning Springbot. While some of these are not technologies I use
-          everyday I am quick to learn and adapt to the needs of the project.
+          My frontend technologies of choice is ReactJS amd TailwindCSS but I do
+          have some experience with VueJS as well. For DBMS I have experience in
+          both SQL or NOSQL databases using MongoDB and Postgresql. For
+          serverside development I have experience in NodeJS, Golang and I am
+          currently learning Springbot. While some of these are not technologies
+          I use everyday I am quick to learn and adapt to the needs of the
+          project.
         </p>
       </div>
     </div>
