b2b637a61f383821a0312a5b1a2bad4542fdd718
khareenp <khareenp1@gmail.com>
2023-04-09 04:36:25 -0400

    add project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import { SocialLinks } from "./components/SocialLinks";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+
+      <SocialLinks />
+    </div>
+  );
+}
